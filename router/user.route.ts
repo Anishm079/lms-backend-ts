2ca4c995a4bf7f7a588e5d7486c8230e15a9b781
Anishahammad Mulla <anishmulla7@gmail.com>
2024-02-12 00:08:54 +0530

    added login logout account activation and verification code

--- router/user.route.ts
+++ router/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { activateUser, loginUser, registrationUser } from "../controllers/user.controller"
+import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller"
 const router = express.Router();
 
 router.post(`/registration`,registrationUser);
 router.post(`/activate-user`,activateUser);
 router.post(`/login-user`,loginUser);
+router.post(`/logout-user`,logoutUser);
 
 export const userRouter = router 
